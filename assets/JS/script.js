84be55507f7262e46e213cf456b029174d296da0
Michael O'Connor <oconnor97@aol.com>
2021-03-04 10:32:14 -0500

    creating a second api call for the uv index and creating if statments to add classes depending on the uvIndex

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -7,10 +7,11 @@ var temp = $('#temp');
 var humid = $('#humid');
 var wind = $('#wind');
 var uvIndex = $('#uvIndex');
+var uviNumber = $('#uviNumber');
 
 // api call
 
-// var getUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.val()+'&appid=0ecab4d27a41d8e0ccd885f7bc5922d7'
+// var getUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.val()+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7'
 
 searchBtn.on('click', function(event) {
   event.preventDefault();
@@ -21,15 +22,29 @@ searchBtn.on('click', function(event) {
     .then(response => response.json())
     .then(data => {
       console.log(data)
-      // var cityNameValue = data[7]
       cityName.text(data.name + " " + moment().format('L'));
       temp.text('Temerature: ' + data.main.temp + ' ℉');
       humid.text('Humidity: ' + data.main.humidity + ' %');
       wind.text('Wind Speed: ' + data.wind.speed + ' MPH');
       var longitude = data.coord.lon;
       var lattitude = data.coord.lat;
+      fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lattitude+'&lon='+longitude+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          uviNumber.text(data.current.uvi);
+        })
+        if(uviNumber.val() <= 3) {
+          uviNumber.addClass('favorable');
+        } else if (uviNumber.val() > 3 && uviNumber < 7) {
+          uviNumber.addClass('moderate');
+        } else {
+          uviNumber.addClass('severe')
+        };
+      
       console.log(lattitude, longitude);
     })
+
   
     .catch(() => {
       console.error('not a city');

f3cc518d7e3e069fa87bf829f1864f413623e8ba
Michael O'Connor <oconnor97@aol.com>
2021-03-04 11:24:55 -0500

    debugging the uvIndex if statement to set the class based on the returned number

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,17 +8,30 @@ var humid = $('#humid');
 var wind = $('#wind');
 var uvIndex = $('#uvIndex');
 var uviNumber = $('#uviNumber');
+var searchHistory = $('#history');
 
-// api call
 
-// var getUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.val()+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7'
+// When the search button is clicked it will run the search function 
+searchBtn.on('click', search);
 
-searchBtn.on('click', function(event) {
+
+// function to retrieve data from the api
+
+function search(event) {
   event.preventDefault();
-   
-  console.log(inputValue.val())
-  
-  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.val()+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7')
+localStorage.setItem('location', inputValue.val());
+for( var i = 0; i > 7; i++) {
+
+}
+
+// api url for the initial fetch request
+var getUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.val()+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7'
+
+console.log(inputValue.val())
+
+//grabbing the data and setting the values to HTML elements 
+
+fetch(getUrl)
     .then(response => response.json())
     .then(data => {
       console.log(data)
@@ -28,15 +41,24 @@ searchBtn.on('click', function(event) {
       wind.text('Wind Speed: ' + data.wind.speed + ' MPH');
       var longitude = data.coord.lon;
       var lattitude = data.coord.lat;
-      fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+lattitude+'&lon='+longitude+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7')
+
+
+      // api url for the fetch request returning the uv index
+      var uviUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lattitude+'&lon='+longitude+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7';
+
+      //Getting the uvIndex from a second api and setting the value to HTML elements, then chaning the class based on the number. 
+
+      fetch(uviUrl)
         .then(response => response.json())
         .then(data => {
           console.log(data);
           uviNumber.text(data.current.uvi);
         })
-        if(uviNumber.val() <= 3) {
+        console.log(uviNumber);
+
+        if(uviNumber.val() < 3) {
           uviNumber.addClass('favorable');
-        } else if (uviNumber.val() > 3 && uviNumber < 7) {
+        } else if (uviNumber.val() > 3 && uviNumber.val() < 7) {
           uviNumber.addClass('moderate');
         } else {
           uviNumber.addClass('severe')
@@ -50,4 +72,4 @@ searchBtn.on('click', function(event) {
       console.error('not a city');
   })
   
-});
+};

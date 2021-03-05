696883621ed354683dadda8e5d9d087ddf8f5ea8
Michael O'Connor <oconnor97@aol.com>
2021-03-04 21:53:12 -0500

    debugging uvIndex

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,19 +10,42 @@ var uvIndex = $('#uvIndex');
 var uviNumber = $('#uviNumber');
 var searchHistory = $('#history');
 
+var cities = JSON.parse(localStorage.getItem('cities')) || [];
 
 // When the search button is clicked it will run the search function 
 searchBtn.on('click', search);
 
 
-// function to retrieve data from the api
+var searchList = $(".history");
+function getHistory() {
+  console.log(searchList);
+  // for (var i = 0; i < localStorage.length; i++) {
 
+  //   var cityHistory = localStorage.getItem(i);
+  //   searchList.append("<li>" + cityHistory + "</li>");
+  // }
+  var histories =JSON.parse(localStorage.getItem('cities'));
+  console.log(histories);
+  if (histories) {
+    histories.forEach(history => {
+      searchList.append(`<button>${history}</button>`);
+    })
+  }
+};
+
+getHistory();
+// function to retrieve data from the api
 function search(event) {
   event.preventDefault();
-localStorage.setItem('location', inputValue.val());
-for( var i = 0; i > 7; i++) {
+  
+cities.push(inputValue.val());
 
-}
+localStorage.setItem('cities', JSON.stringify(cities));
+
+searchList.empty();
+
+
+getHistory();
 
 // api url for the initial fetch request
 var getUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.val()+'&units=imperial&appid=0ecab4d27a41d8e0ccd885f7bc5922d7'
@@ -34,8 +57,15 @@ console.log(inputValue.val())
 fetch(getUrl)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log(data);
       cityName.text(data.name + " " + moment().format('L'));
+
+      // weather icon
+      
+      $('#wIcon').show()
+      var iconCode = data.weather[0].icon;
+      var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+      $('#wIcon').attr('src', iconUrl);
       temp.text('Temerature: ' + data.main.temp + ' ℉');
       humid.text('Humidity: ' + data.main.humidity + ' %');
       wind.text('Wind Speed: ' + data.wind.speed + ' MPH');
@@ -55,10 +85,11 @@ fetch(getUrl)
           uviNumber.text(data.current.uvi);
         })
         console.log(uviNumber);
-
-        if(uviNumber.val() < 3) {
+        let uvVal = parseFloat(uviNumber.textContent)
+        console.log(uvVal)
+        if(uvVal < 3) {
           uviNumber.addClass('favorable');
-        } else if (uviNumber.val() > 3 && uviNumber.val() < 7) {
+        } else if (uvVal > 3 && uvVal < 7) {
           uviNumber.addClass('moderate');
         } else {
           uviNumber.addClass('severe')
